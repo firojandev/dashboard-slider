73c2f91b44a993b0d874c4eff69d244d25ef28a4
Md Altaf Hoshain <altaf@squaregroup.com>
2024-03-19 13:03:56 +0600

    test file is updated

--- test/dashboard_slider_test.dart
+++ test/dashboard_slider_test.dart
@@ -1,12 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dashboard_slider/dashboard_slider.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  
 }
